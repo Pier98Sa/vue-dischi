5fb1aabfd00281305c2d23dfd07057b249b4c3c4
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-23 16:34:51 +0100

    MyHeader completato

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="d-flex align-items-center">
+    <img class="img-fluid p-2" alt="spotify logo" src="../assets/Spotify_Logo.png">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  @import '../style/variables.scss';
+
+  header{
+    background-color: $primaryColor;
+    height: 100px;
+
+    img{
+      height: 80px;
+    }
+  }
+
+
+</style>
