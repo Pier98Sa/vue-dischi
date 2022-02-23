17d0c4b46ea9115554b761f6e07243f9dae40346
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-23 18:02:18 +0100

    Esercizio completato

---
+++ src/components/MyMain.vue
@@ -0,0 +1,61 @@
+<template>
+  <main>
+    <div class="container pt-5">
+      <div class="row">
+        <CoverAlbum
+          v-for="(album, index) in listaAlbum"
+          :key="index"
+          :album="album"
+        />
+
+      </div>
+
+    </div>
+
+  </main>
+</template>
+
+<script>
+
+const axios = require('axios');
+
+import CoverAlbum from './partials/CoverAlbum.vue'
+
+export default {
+  name: 'MyMain',
+  data(){
+    return{
+      listaAlbum: [],
+      api: "https://flynn.boolean.careers/exercises/api/array/music"
+    }
+  },
+  components:{
+    CoverAlbum,
+  },
+  methods:{
+    getAlbum(){
+      axios.get(this.api)
+      .then((response) => {
+        this.listaAlbum = response.data.response;
+        console.log(this.listaAlbum);
+      });
+    }
+  },
+  created(){
+      this.getAlbum();
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  @import '../style/variables.scss';
+    main{
+      background-color: $secondaryColor;
+      height: calc(100vh - 100px);
+      overflow-y: auto;
+    }
+
+
+</style>
---
+++ src/components/partials/CoverAlbum.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="col p-3" >
+        <div class="box d-flex flex-column  ">
+            <div class="cover p-3">
+                <img class="img-fluid" :src="album.poster">
+            </div>
+
+            <div class="cover-text text-center">
+                <h2 class="text-white text-uppercase fs-4 p-3">{{album.title}}</h2>
+                <div class="ms_color-text">{{album.author}}</div>
+                <div class="ms_color-text">{{album.year}}</div>
+            </div>
+            
+            
+        </div>
+        
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'CoverAlbum',
+    props:{
+        "album": Object,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import '../../style/variables.scss';
+
+.box{
+    width: 200px;
+    height: 360px;
+    background-color: $primaryColor;
+
+    .cover{
+        height: 50%;
+    }
+
+    .cover-text{
+        height: 50%;
+    }
+
+    .ms_color-text{
+        color: $tertiaryColor;
+    }
+        
+}
+</style>
